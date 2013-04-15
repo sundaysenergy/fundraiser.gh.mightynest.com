74cf66dd2e136f64a64203b7911b8e603d18f24b
Dmitry Evseev <kradmiy@gmail.com>
2013-04-15 21:03:36 +0300

    Fires up mightynestApp angular module

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 'use strict';
 
 /* App Module */
+angular.module('mightynestApp', [])
+  .config(function ($routeProvider) {
 
-/* Routes */
-
-/* Route for getting school_id into parameter */
-
-/* Default */
+    /* Routes */
+    $routeProvider
+      /* Route for getting school_id into parameter */
+      .when('/school/:schoolId', {
+        templateUrl: 'views/school.html',
+        controller: 'SchoolCtrl'
+      })
+      /* Default route */
+      .when('/', {
+        templateUrl: 'views/form.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
