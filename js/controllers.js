50a9195cdd0ee6ad79f9feb1365290b9e3aeb5e8
Kai Curry <kai@sundaysenergy.com>
2013-04-15 15:56:41 -0500

    remove extra comments

--- js/controllers.js
+++ js/controllers.js
@@ -18,42 +18,19 @@ angular.module('mightynestApp')
     // prepare response to be passed to Wufoo
     var mapResponseToWufoo = function (properties) {
       var mapping = {
-          // school_name =  organization  = Field12
+          //wufooFiedId : jsonFileFieldId
           'Field12': 'school_name',
-          //  MS school_id  = Field20
           'Field20': 'school_id',
-          //  MS Affiliate ID  = Field23
-          // MS self-signup = 'more info',
-          //'Field24': '',
-          //mail_address  = Field25
           'Field25': 'mail_address',
-          // mail_city = Field26
           'Field26': 'mail_city',
-          // mail_state = Field27
           'Field27': 'mail_state',
-          // mail_zip = Field28
           'Field28': 'mail_zip',
-          // mail_zip4 = Field29
           'Field29': 'mail_zip4',
-          //  school phone  = Field30
           'Field30': 'phone',
-          //  MS School Size  = Field31
           'Field31': 'num_students',
-          // MS Grade - start  = Field32
           'Field32': 'lowest_grade',
-          //  MS Grade - end  = Field33
           'Field33': 'highest_grade',
-          //  MS School Type  = Field34
           'Field34': 'source'
-          //  MS Sign-up Date  = Field35
-          //'Field35':
-          //  MS Start Year  = Field36
-          //'Field36':
-          //  MS URL  = Field21
-          //  Lead Source  = Field37
-          //'Field37': 'mightynest.com',
-          // Lead Medium  = Field38
-          //'Field38': 'self-signup'
         },
         prepopulated = {
           'Field23': $scope.schoolId,
