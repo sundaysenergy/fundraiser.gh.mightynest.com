0ffc0c1d5f43d18f8b8b6f3fced35c0d796af252
Dmitry Evseev <kradmiy@gmail.com>
2013-04-29 22:55:34 +0300

    Enables header to be loaded from github

--- js/config.js
+++ js/config.js
@@ -4,6 +4,11 @@
   // 42 http://mightynest.com/mightynest/user/json
 mightynestApp
   .constant('config', {
+    URLS: {
+      //https://api.github.com/repos/westbroadway/northmpls_content/contents/business.json
+      HEADER_CONTENT: 'https://api.github.com/repos/MightyNest/mightyschool_content/contents/fundraiser_header.yml'
+    },
+    GITHUB_HEADERS: {headers: {"Accept": "application/vnd.github.raw"}},
     JSON_API_DOMAIN: 'http://mightynest.com/',
     FAQ_URL: 'http://github.webscript.io/dir_content?repo=mightynest/mightyschool_content&dir=faq'
   });
